fcf17a6a1a33c51022cf1272b4ca0ea848ff1cec
Nicolas <nlotruglio@gmail.com>
2022-06-15 23:37:39 -0400

    Update project_one.py
    Added the function to add customer info, currently working on how to link databases through keys for the Orders table

--- project_one.py
+++ project_one.py
@@ -11,8 +11,10 @@ mydb = mysql.connector.connect(
 
 mycursor = mydb.cursor()
 
+
+
 #creating tables
-#mycursor.execute("CREATE TABLE Games (GameID int PRIMARY KEY AUTO_INCREMENT, Title VARCHAR(255), Platform VARCHAR(255), Price VARCHAR(50))") 
+#mycursor.execute("CREATE TABLE Customers (CustomerID int PRIMARY KEY AUTO_INCREMENT, Name VARCHAR(255) , Address VARCHAR(255), Phone VARCHAR(50))") 
 
 #Adding a column to a table
 #mycursor.execute("ALTER Games ADD Price DECIMAL")
@@ -20,10 +22,10 @@ mycursor = mydb.cursor()
 #Inserting values into a table
 #mycursor.execute("INSERT INTO Customers (name, address, phone) VALUES ('Anthony Taylor', '193 Coffey Creek Suite 064 Port David DE 38909', '700-409-7604')")
 
-# mycursor.execute("INSERT INTO Orders (Game, Customer, Price) VALUES ('Mega-Man', 'David Nagel', 5.00)")
-# mycursor.execute("INSERT INTO Customers (Name, Address, Phone) VALUES ('David Nagel', '441 Margaret Loop Whiteland GA 11670', '374-343-7974')")
-#mycursor.execute("INSERT INTO Games (Title, Platform, Price) VALUES ('Mega-Man', 'Playstation', '4.99')")
-#mydb.commit()
+#mycursor.execute("INSERT INTO Orders (Game, Customer, Price) VALUES ('Mega-Man', 'David Nagel', 5.00)")
+mycursor.execute("INSERT INTO Customers (Name, Address, Phone) VALUES ('Anthony Taylor', '441 Oakridge Lane', '847-273-9839')")
+mycursor.execute("INSERT INTO Games (Title, Platform, Price) VALUES ('Final Fantasy XIV', 'PC', '59.99')")
+mydb.commit()
 
 
 
@@ -36,49 +38,69 @@ mycursor = mydb.cursor()
 #     mycursor.execute("INSERT INTO Games (Title, Platform, Price) VALUES ('Mega-Man', 'Playstation', 5.00)")
 
 
-# def addCustomer():
-#     print("Please input Customer information:")
-#     mycursor.execute("INSERT INTO Customers (Name, Address, Phone) VALUES ('David Nagel', '441 Margaret Loop Whiteland GA 11670', '374-343-7974')") 
+# def customerInfo():
+#     cusName = str(input('Please enter your full name: '))
+#     cusAddress = str(input('Please enter your address: '))
+#     cusPhone = str(input('Please enter your number: '))
+
+#     mycursor.execute("INSERT INTO Customers (Name, Address, Phone) VALUES (%s, %s, %s)", (cusName, cusAddress, cusPhone))
+    
+
 
 # def addOrder():
 #     mycursor.execute("INSERT INTO Orders (Game, Customer, Price) VALUES ('Mega-Man', 'David Nagel', 5.00)")
 
 
 
-def viewGames():
-    allGames =  mycursor.execute("SELECT Title, Price FROM Games ORDER BY Title ASC")
-    for allGames in mycursor:
-        print(allGames)
+# def viewGames():
+#     allGames =  mycursor.execute("SELECT Title, Platform, Price FROM Games ORDER BY Title ASC")
+#     for allGames in mycursor:
+#         print(allGames)
 
-#viewGames()
 
-def startup():
-    while True:
-        print("Welcome to Nick's online game store!")
-        print('Please input an option from the menu!')
-        print('\t1. View current game selection')
-        print('\t2. Place an order')
-        print('\t3. View customer info')
-        print('\t4. Add a game')
-        print('\t5. Exit')
-        break
-    while True:
-        try:
-            sel = int(input("\nSelection: "))  
-        except ValueError:
-            print(ValueError)
-            print("Not valid input")
-        if sel > 5:
-            print("Not a valid input")
-        else:
-            break
-    if sel == 1:
-        print("Our current selection of games")
-        viewGames()
+
+# def startup():
+#     while True:
+#         print("Welcome to Nick's online game store!")
+#         print('Please input an option from the menu!')
+#         print('\t1. View current game selection')
+#         print('\t2. Add customer info')
+#         print('\t3. Place an order')
+#         print('\t4. View order')
+#         print('\t5. Cancel an order')
+#         print('\t6. Exit')
+#         break
+#     while True:
+#         try:
+#             sel = int(input("\nSelection: "))  
+#         except ValueError:
+#             print(ValueError)
+#             print("Not valid input")
+#         if sel > 6:
+#             print("Not a valid input")
+#         else:
+#             break
+#     if sel == 1:
+#         print("Our current selection of games")
+#         viewGames()
+#         print("Would you like to make another selection?")
+#         print('\t2. Place an order')
+#         print('\t3. View customer info')
+#         print('\t4. Add a game')
+#         print('\t5. Exit')
+#         sel = int(input("\nSelection: "))
+#     elif sel == 2:
+#         print("Please enter your customer information")
+#         customerInfo()
+#         mydb.commit()
+#     elif sel == 3:
+#         print()
+
+
+    
     
 
-    
 
 
-startup()
+# startup()
 
